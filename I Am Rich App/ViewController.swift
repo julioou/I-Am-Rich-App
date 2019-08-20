77d6b5fdd4a9f5c966aa976f57f9fd890a6bfba8
tes <tes>
2019-08-20 14:48:38 -0300

    My First Commit

---
+++ I Am Rich App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich App
+//
+//  Created by Treinamento on 8/13/19.
+//  Copyright © 2019 trainee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
